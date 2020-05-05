cebc7366e200da25ad5178882c1aa6d9a843e5cf
LaraSMota <lara.s.mota@gmail.com>
2020-05-05 10:26:56 +0100

    Correção de cor dos hints dos formulários, criação da tela de deltar e criação do evento para adicionar card

--- app/src/main/java/com/example/cardbe/DeleteAccount.java
+++
@@ -1,4 +0,0 @@
-package com.example.cardbe;
-
-public class DeleteAccount {
-}
--- app/src/main/java/com/example/cardbe/DeleteAccount.kt
+++ app/src/main/java/com/example/cardbe/DeleteAccount.kt
@@ -1,25 +1,27 @@
 package com.example.cardbe
 
-import android.content.Intent
 import android.graphics.PorterDuff
 import android.graphics.PorterDuffColorFilter
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageButton
-import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.core.content.res.ResourcesCompat
 import androidx.core.view.children
 
-class Board : AppCompatActivity() {
+class DeleteAccount  : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_board)
-        val linearLayout = findViewById<LinearLayout>(R.id.BoardLinearLayoutHorizontal)
+        setContentView(R.layout.activity_deleteaccount)
+        val cancelButton = findViewById<Button>(R.id.DeleteCancel)
+        val confirmButton = findViewById<Button>(R.id.DeleteConfirm)
 
         //Set back button and back button color
-        var toolbar = findViewById<Toolbar>(R.id.BoardToolbar)
+        var toolbar = findViewById<Toolbar>(R.id.DeleteAccountToolbar)
         setSupportActionBar(toolbar)
 
         val myColorFilter = PorterDuffColorFilter(ResourcesCompat.getColor(resources, R.color.button, null), PorterDuff.Mode.MULTIPLY)
@@ -32,10 +34,23 @@ class Board : AppCompatActivity() {
             }
         }
 
-        linearLayout.children.forEach {
-            val linerLayoutVertical = it as LinearLayout
-            linerLayoutVertical.getChildAt(1).setOnClickListener{
-                startActivity(Intent(this, CreateCard::class.java))
+        cancelButton.setOnClickListener{
+            finish()
+        }
+
+        confirmButton.setOnClickListener{
+            //Apagar usuario do banco
+            val password = "Teste01"
+            val confirmPassword = findViewById<EditText>(R.id.DeleteAccountConfirmNewPassword)
+            if(password == confirmPassword.text.toString()){
+
+                super.finish()
+            } else {
+                Toast.makeText(
+                    applicationContext,
+                    "Password don't match",
+                    Toast.LENGTH_LONG
+                ).show()
             }
         }
     }
